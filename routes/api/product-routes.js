34f37b16411807626e433001d98f792ce07a17cc
qgtere <86640621+qgtere@users.noreply.github.com>
2022-09-16 22:07:07 -0600

    CRUD for Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // include its associated Category and Tag data
   try {
     const productsData = await Product.findAll({
       include: [{ model: Category }, { model: Tag, through: ProductTag }]
@@ -22,10 +19,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
+// get one product by id
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag, through: ProductTag }]
